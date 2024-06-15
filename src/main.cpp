4cef2abe528c76fee0ef0b3f592bf226d5e114c4
Matthew Kolega <12107095+mattkolega@users.noreply.github.com>
2024-06-15 14:11:26 +1000

    Implement image output

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,36 @@
+#include <array>
 #include <iostream>
 
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
 int main(int argc, char** argv) {
-    std::cout << "hello world!" << std::endl;
-    return 0;
+    constexpr int IMAGE_WIDTH = 256;
+    constexpr int IMAGE_HEIGHT = 256;
+    constexpr int CHANNEL_NUM = 3;  // Image is in RGB format
+
+    std::array<unsigned char, IMAGE_WIDTH * IMAGE_HEIGHT * CHANNEL_NUM> pixelData {};
+
+    int pixelDataIndex = 0;
+
+    for (int i = 0; i < IMAGE_HEIGHT; i++) {
+        std::clog << "\rScanlines remaining: " << (IMAGE_HEIGHT - i) << ' ' << std::flush;
+        for (int j = 0; j < IMAGE_WIDTH; j++) {
+            auto r = double(j) / (IMAGE_WIDTH - 1);
+            auto g = double(i) / (IMAGE_HEIGHT - 1);
+            auto b = 0.0;
+
+            int ir = int(255.999 * r);
+            int ig = int(255.999 * g);
+            int ib = int(255.999 * b);
+
+            pixelData[pixelDataIndex++] = ir;
+            pixelData[pixelDataIndex++] = ig;
+            pixelData[pixelDataIndex++] = ib;
+        }
+    }
+
+    stbi_write_png("image.png", IMAGE_WIDTH, IMAGE_HEIGHT, CHANNEL_NUM, pixelData.data(), IMAGE_WIDTH * CHANNEL_NUM);
+
+    std::clog << "\rDone.                 \n";
 }
